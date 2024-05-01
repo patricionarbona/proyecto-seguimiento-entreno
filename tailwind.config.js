77fadf5ea7b07e25223e08e8f6aaa190473aab86
patricionarbona <patricioinarbona@gmail.com>
2024-05-01 18:06:29 +0200

    eliminados colores personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        'gym1' : '#222831',
-        'gym2' : '#31363F',
-        'gym3' : '#76ABAE',
-        'gym4' : '#EEEEEE'
+      
       }
     },
   },
