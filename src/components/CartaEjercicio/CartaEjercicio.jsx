0ea847fcae827e903cd5b61102e9b2ebe3461fef
patricionarbona <patricioinarbona@gmail.com>
2024-05-01 22:37:36 +0200

    creado estilo basico de CartaEjercicio

--- src/components/CartaEjercicio/CartaEjercicio.jsx
+++ src/components/CartaEjercicio/CartaEjercicio.jsx
@@ -4,12 +4,13 @@ import InputNum from "../InputNum/InputNum"
 function CartaEjercicio() {
 
     const rutaImg = "https://v2.exercisedb.io/image/I4XMjCBFhqaGoJ"
+    const nombreEjercicio = "alternate lateral pulldown"
 
     return(
         <>
-            <div>
+            <div className="relative bg-slate-200 h-96 w-80 flex flex-col items-center justify-center rounded-2xl">
                 <h2>alternate lateral pulldown</h2>
-                <img src={rutaImg} alt="" />
+                <img className="h-5 w-5" src={rutaImg} alt={"un hombre haciendo " + nombreEjercicio}/>
                 <h3>Observaciones del ejercicio</h3>
                 <p>Hacerlo con cariño</p>
                 <div>
