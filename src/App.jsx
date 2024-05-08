9042a13e075a2a0000dbfa47afd08de79e5becce
patricionarbona <patricioinarbona@gmail.com>
2024-05-08 22:10:36 +0200

    barra de navegacion y react router hecho

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import './App.css'
 import Login from './components/Login/Login.jsx'
 import ExerciseSelector from './components/ExerciseSelector/ExerciseSelector.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from "./pages/HomePage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import SelectorPage from "./pages/SelectorPage.jsx"
+import EntrenosPage from "./pages/EntrenosPage.jsx"
+import NotFoundPage from "./pages/NotFoundPage.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage/>,
+    errorElement: <NotFoundPage/>
+  },
+  {
+    path: '/login',
+    element: <LoginPage/>
+  },
+  {
+    path: '/selector',
+    element: <SelectorPage/>
+  },
+  {
+    path: '/entrenos',
+    element: <EntrenosPage/>
+  }
+])
+
 
 function App() {
 
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,15 @@
+import InputTexto from "../components/InputTexto/InputTexto"
+
 export default function LoginPage() {
     return (
         <div>
-            Soy Login
+            <div className="absolute bg-slate-200 h-96 w-72 xl:w-80 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-8 rounded-2xl">
+                <InputTexto estilo={"w-52 rounded-md ring-1 ring-gray-200"} nombre={"Email"} id={"inputEmail"}/>
+                <InputTexto estilo={"w-52 rounded-md ring-1 ring-gray-200"} nombre={"Contraseña"} id={"inputContraseña"}/>
+                <button className="bg-gray-800 text-gray-100 px-4 py-2 rounded-lg text-center hover:bg-gray-700 ring-1 ring-gray-500 mt-10">
+                    Iniciar Sesión
+                </button>
+            </div>
         </div>
     )
 }
