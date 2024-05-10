e64f0ac9e3baa2c97ea4cf5d1440bd09fd6c43ad
patricionarbona <patricioinarbona@gmail.com>
2024-05-10 23:18:39 +0200

    carta voltea

--- src/components/CartaEjercicio/CartaEjercicio.jsx
+++ src/components/CartaEjercicio/CartaEjercicio.jsx
@@ -2,29 +2,30 @@ import InputNum from "../InputNum/InputNum"
 
 
 function CartaEjercicio({estilo =""}) {
-
+    const nombreEjercicio = "jalon pecho unilateral"
     const rutaImg = "https://v2.exercisedb.io/image/I4XMjCBFhqaGoJ"
-    const nombreEjercicio = "alternate lateral pulldown"
-
-    return(
-        <>
-            <div 
-                className= {
-                    "relative bg-slate-200 h-96 w-80 flex flex-col items-center justify-center rounded-2xl shrink-0"
-                    + estilo
-            }>
-                <h2>alternate lateral pulldown</h2>
-                <img className="h-28 w-28" src={rutaImg} alt={"un hombre haciendo " + nombreEjercicio}/>
-                <h3>Observaciones del ejercicio</h3>
-                <p>Hacerlo con cariño</p>
-                <div>
-                    <InputNum id={"inputPeso"} nombre={"Peso"} />
+    const musculo = "espalda alta"
+    const recomendacion = [
+        "Start by hanging from a pull-up bar with an overhand grip, hands slightly wider than shoulder-width apart.",
+        "Engage your core and pull your shoulder blades down and back.",
+        "Bend your knees and tuck them towards your chest.",
+        "Slowly lift your legs up, keeping them straight, until your body is parallel to the ground.",
+        "Hold this position for a few seconds, then slowly lower your legs back down to the starting position.",
+        "Repeat for the desired number of repetitions."
+    ]
+    return (
+        <div className="flex min-h-screen flex-col justify-center bg-slate-100">
+            <div className="group h-96 w-96 [perspective:1000px]">
+                <div className="relative h-full w-full rounded-xl shadow-xl [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] [backface-visibility:hidden]">
+                    <div className="absolute inset-0">
+                        A
+                    </div>
+                    <div className="absolute inset-0 h-full w-full rounded-xl shadow-xl [transform:rotateY(180deg)] [backface-visibility:hidden]">
+                        B
+                    </div>
                 </div>
-                <h3>Observaciones del entreno</h3>
-                    <textarea name="" id="" cols="10" rows="4"></textarea>
-                <button className="bg-gray-800 text-gray-100 px-4 py-2 rounded-lg text-center hover:bg-gray-700 ring-1 ring-gray-500 mt-10" >Hecho</button>
             </div>
-        </>
+        </div>
     )
 }
 export default CartaEjercicio
