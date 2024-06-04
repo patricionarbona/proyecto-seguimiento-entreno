7f7f08c8d109cde926e6413476b3e4c800a1e14c
patricionarbona <patricioinarbona@gmail.com>
2024-06-04 09:09:44 +0200

    create elements of ManageExercises

--- src/pages/ManageExercises.jsx
+++ src/pages/ManageExercises.jsx
@@ -1,7 +1,67 @@
-export default function ManageExercises() {
-    return(
-        <div>
-            maneja ejercicios
-        </div>
-    )
+import { useContext, useEffect, useState } from "react";
+import NavDesktop from "../components/NavDesktop/NavDesktop";
+import { recuperarEjercicios } from "../utils/Peticiones";
+import MainContext from "../context/MainContext";
+import toast from "react-hot-toast";
+
+export default function ManageUsers() {
+    const [ejercicios, setEjercicios] = useState([]);
+
+
+  useEffect(() => {
+    const fetchEjercicios = async () => {
+      try {
+        const response = await recuperarEjercicios();
+        setEjercicios(response);
+        console.log(response)
+      } catch (err) {
+        console.error("Error al recuperar los ejercicios:", err);
+      }
+    };
+
+    fetchEjercicios();
+  }, []);
+
+  return (
+    <div className="flex flex-col h-[100vh]">
+      <NavDesktop />
+      <div className="bg-teal-400 h-full mt-14">
+        <table
+            className="table-auto"
+        >
+          <thead>
+            <th>Nombre</th>
+            <th>Equipamiento</th>
+            <th>Ejecución</th>
+            <th>Foto</th>
+            <th>Grupo muscular principal</th>
+            <th>Musculos secundarios</th>
+            <th>Acciones</th>
+          </thead>
+          <tbody>
+            {ejercicios.map((ejercicio) => {
+                return (
+                    <tr key={`exc-${ejercicio.id}`}>
+                      <td>{ejercicio.ejercicio}</td>
+                      <td>{ejercicio.equipamiento}</td>
+                      <td>{ejercicio.descripcion}</td>
+                      <td>{ejercicio.foto}</td>
+                      <td>{ejercicio.grupo}</td>
+                      <td>{ejercicio.musculos}</td>
+                      <td>
+                        <button 
+                            // onClick={() => handleClickDelete(ejercicio.id)}
+                            >
+                          Eliminar
+                        </button>
+                        <button>Editar</button>
+                      </td>
+                    </tr>
+                  );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
