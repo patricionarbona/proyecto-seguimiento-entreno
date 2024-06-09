8d11813911e9c8eb1d75c2e97288a8f21e06536c
patricionarbona <patricioinarbona@gmail.com>
2024-06-09 19:48:22 +0200

    fix logo link

--- src/components/NavDesktop/NavDesktop.jsx
+++ src/components/NavDesktop/NavDesktop.jsx
@@ -20,7 +20,7 @@ export default function NavDesktop() {
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link
-            to="/"
+            to="/front-page"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
