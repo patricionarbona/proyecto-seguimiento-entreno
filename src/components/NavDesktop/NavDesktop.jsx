486cc4dcfd9dfd296fbfee4478ff870021090336
patricionarbona <patricioinarbona@gmail.com>
2024-06-11 10:51:26 +0200

    feat add logo

--- src/components/NavDesktop/NavDesktop.jsx
+++ src/components/NavDesktop/NavDesktop.jsx
@@ -24,8 +24,8 @@ export default function NavDesktop() {
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
+              src="/img/logo.png"
+              className="h-16"
               alt="Flowbite Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -5,7 +5,7 @@ export default function FrontPage() {
     return(
         <>
             <NavDesktop />
-            <div className="mt-16 flex flex-col">
+            <div className="mt-24 flex flex-col">
                 <div className="h-[47vh] flex flex-col sm:flex-row justify-between mt-4 w-11/12 mx-auto ">
                     <div className="bg-slate-100 h-full w-3/12 rounded-2xl shadow-sm ">EJERCICIO</div>
                     <div className="bg-slate-100 h-full w-7/12 rounded-2xl shadow-sm ">GRAFICA</div>
